3e463dd22a409181dc4bf53797bca01f12002415
Yuvraj <1406yuvraj@gmail.com>
2026-04-08 19:35:34 +0530

    task7 completed

--- task6/task7/script.js
+++ task6/task7/script.js
@@ -1,6 +1,6 @@
 let posts = [];
 
-// Fetch data once
+
 async function fetchData() {
     try {
         const response = await fetch("https://jsonplaceholder.typicode.com/posts");
@@ -13,7 +13,7 @@ async function fetchData() {
     }
 }
 
-// Display function
+
 function displayData(data) {
     const list = document.getElementById("postList");
     list.innerHTML = "";
@@ -25,7 +25,7 @@ function displayData(data) {
     });
 }
 
-// Search function
+
 function searchData() {
     const value = document.getElementById("searchInput").value.toLowerCase();
 
@@ -36,5 +36,5 @@ function searchData() {
     displayData(filtered);
 }
 
-// Call fetch on page load
+
 fetchData();
